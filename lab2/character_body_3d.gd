d6aef08034ca8e9a88e35dbb0ada6c0be64bc36d
DESKTOP-9PHC34C\P-Mac <mariusmeik@gmail.com>
2023-11-18 02:00:13 +0200

    working game

--- lab2/character_body_3d.gd
+++ lab2/character_body_3d.gd
@@ -1,11 +1,15 @@
 extends CharacterBody3D
 
-const MOVE_SPEED = 1
+const MOVE_SPEED = 5
+const JUMP_VELOCITY = 8
  
 @onready var raycast = $RayCast3D
 @onready var anim_player = $AnimationPlayer
 @onready var zombieDeathSound = $zombieDiesSound
 @onready var kraujas =$kraujas
+@onready var timer = $Timer
+
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
  
 var player = null
 var dead = false
@@ -15,15 +19,24 @@ func _ready():
 	add_to_group("zombies")
  
 func _physics_process(delta):
-	if dead:
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	if dead or player == null:
 		return
-	if player == null:
-		return
- 
-	var vec_to_player = player.position - position
+
+	var vec_to_player = player.global_transform.origin - global_transform.origin
 	raycast.target_position = vec_to_player.normalized() * 1.5
- 
-	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+
+	var direction = vec_to_player.normalized()
+	if direction.length_squared() > 0.01:  # Check if the direction is not close to zero
+		velocity.x = direction.x * MOVE_SPEED
+		velocity.z = direction.z * MOVE_SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, MOVE_SPEED)
+		velocity.z = move_toward(velocity.z, 0, MOVE_SPEED)
+
+	move_and_slide()
  
 	if raycast.is_colliding():
 		var coll = raycast.get_collider()
@@ -40,3 +53,8 @@ func kill():
  
 func set_player(p):
 	player = p
+	
+func _on_timer_timeout():
+	velocity.y = JUMP_VELOCITY
+	timer.wait_time = randf_range(5, 15)
+	timer.start()
--- lab2/levels/CharacterBody3D.gd
+++ lab2/levels/CharacterBody3D.gd
@@ -1,12 +1,17 @@
 extends CharacterBody3D
- 
-const MOVE_SPEED = 4
-const MOUSE_SENS = 0.5
+const SPEED = 5.0
+var mouseSensibility = 1500
+var mouse_relative_x = 0
+var mouse_relative_y = 0
+const JUMP_VELOCITY = 4.5
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
  
 @onready var anim_player = $AnimationPlayer
-@onready var raycast = $RayCast3D
+@onready var raycast = $Camera3D/RayCast3D
 @onready var sprite = $CanvasLayer/Control/victory
 @onready var gunshot =$gunshot
+@onready var Cam = $Camera3D
+@export var _bullet_scene : PackedScene
 
 var killCount = 0
  
@@ -15,10 +20,6 @@ func _ready():
 	await get_tree().process_frame
 	get_tree().call_group("zombies", "set_player", self)
  
-func _input(event):
-	if event is InputEventMouseMotion:
-		rotation_degrees.y -= MOUSE_SENS * event.relative.x
- 
 func _process(delta):
 	if Input.is_action_pressed("exit"):
 		get_tree().quit()
@@ -26,29 +27,55 @@ func _process(delta):
 		kill()
  
 func _physics_process(delta):
-	var move_vec = Vector3()
-	if Input.is_action_pressed("moveUp"):
-		move_vec.z -= 1
-	if Input.is_action_pressed("moveDown"):
-		move_vec.z += 1
-	if Input.is_action_pressed("moveLeft"):
-		move_vec.x -= 1
-	if Input.is_action_pressed("moveRight"):
-		move_vec.x += 1
-	move_vec = move_vec.normalized()
-	move_vec = move_vec.rotated(Vector3(0, 1, 0), rotation.y)
-	move_and_collide(move_vec * MOVE_SPEED * delta)
- 
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	# Handle Shooting
 	if Input.is_action_pressed("Shoot") and !anim_player.is_playing():
-		gunshot.play()
-		anim_player.play("shootAnimation")
-		var coll = raycast.get_collider()
-		if raycast.is_colliding() and coll.has_method("kill"):
-			coll.kill()
-			killCount = killCount + 1
-	if killCount == 4:
-		anim_player.play("win")
-		sprite.visible = true
+		shoot()
  
+	# Get the input direction and handle the movement/deceleration.
+	var input_dir = Input.get_vector("moveLeft", "moveRight", "moveUp", "moveDown")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()
+
+func _input(event):
+	if event is InputEventMouseMotion:
+		rotation.y -= event.relative.x / mouseSensibility
+		Cam.rotation.x -= event.relative.y / mouseSensibility
+		Cam.rotation.x = clamp(Cam.rotation.x, deg_to_rad(-90), deg_to_rad(90) )
+		mouse_relative_x = clamp(event.relative.x, -50, 50)
+		mouse_relative_y = clamp(event.relative.y, -50, 10)
+
 func kill():
 	get_tree().reload_current_scene()
+	
+func shoot():	
+	gunshot.play()
+	anim_player.play("shootAnimation")
+	var coll = raycast.get_collider()
+	if raycast.is_colliding():
+		var bulletInst = _bullet_scene.instantiate() as Node3D
+		bulletInst.set_as_top_level(true)
+		get_parent().add_child(bulletInst)
+		bulletInst.global_transform.origin = raycast.get_collision_point() as Vector3
+		bulletInst.look_at((raycast.get_collision_point()+ raycast.get_collision_normal()),Vector3.BACK)
+		print(raycast.get_collision_point())
+		print(raycast.get_collision_point()+ raycast.get_collision_normal())
+		if coll.has_method("kill"):
+			coll.kill()
+			killCount = killCount + 1
+	if killCount == 8:
+		anim_player.play("win")
+		sprite.visible = true
